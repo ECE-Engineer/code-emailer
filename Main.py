464408ff43a0dfbc7cd0ccada1a01b89b055a016
ECE-Engineer <kzeller@oswego.edu>
2017-04-28 13:37:14 -0400

    file traversal warmup

---
+++ Main.py
@@ -0,0 +1,31 @@
+### create a csc344 directory
+### create a hw 1 - 5 folder
+### find the src folders in the 344 folders
+### copy all the contents of those folders into their own folder labeled hw 1 - 5 (use pattern matching of the file extensions to put the files in the correct folders)
+### 
+
+import os
+import glob
+import sys
+
+path = 
+
+from os.path import join
+for root, dirs, files in os.walk(path):
+	print ("Current directory", root)
+	print ("Sub directories", dirs)
+	print ("Files", files)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
